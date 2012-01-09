cec09d11301421fde35c790b26f18cb04ba20eb2
Nick Recobra <oruenu@gmail.com>
2012-01-09 21:15:08 +0400

    skeleton

---
+++ lib/rails_admin-treeview.rb
@@ -0,0 +1,7 @@
+require "rails_admin-treeview/version"
+
+module RailsAdmin
+  module Treeview
+    # Your code goes here...
+  end
+end
---
+++ lib/rails_admin-treeview/version.rb
@@ -0,0 +1,5 @@
+module RailsAdmin
+  module Treeview
+    VERSION = "0.0.1"
+  end
+end
